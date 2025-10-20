4d4b9c74e578fd09ebc51d8e83ecca303916cb1a
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2025-10-20 17:50:38 +0530

    frontend

---
+++ frontend/src/components/AppBar.jsx
@@ -0,0 +1,20 @@
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export const Nav = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" >
+            Task Manager
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
